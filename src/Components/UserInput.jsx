27de630a9410a49419660303f6c350324d1d09a6
Daniela Damjanoska <daniela_damjanoska@yahoo.com>
2022-07-11 11:57:21 +0200

    some code refactoring made

--- src/Components/UserInput.jsx
+++ src/Components/UserInput.jsx
@@ -3,40 +3,37 @@ import useHandleInputValue from '../Hooks/useHandleInputValue';
 import TextField from '@mui/material/TextField';
 
 export default function UserInput({ onGetUserSuccess, isSignIn }) {
-    const [userSuccess, setUserSuccess] = useState(false),
-        [userErrors, setUserErrors] = useState(false),
+    const [userSuccess, setUserSuccess] = useState(null),
         [helperTextUser, setHelperTextUser] = useState('');
 
     const { value: username, onChange } = useHandleInputValue();
 
     const validateUserSignUp = () => {
-        if (username) {
-            if (
-                !username.match(
-                    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-                )
-            ) {
-                setUserErrors(true);
-                setHelperTextUser('Please enter a valid e-mail address');
-            } else {
-                setUserSuccess(true);
-                localStorage.setItem('username', JSON.stringify(username));
-            }
+        if (!username) return;
+        if (
+            !username.match(
+                /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+            )
+        ) {
+            setUserSuccess(false);
+            setHelperTextUser('Please enter a valid e-mail address');
+        } else {
+            setUserSuccess(true);
+            localStorage.setItem('username', JSON.stringify(username));
         }
     };
 
     const validateUserSignIn = () => {
-        if (username) {
-            const userFromLS = JSON.parse(localStorage.getItem('username'));
+        if (!username) return;
+        const userFromLS = JSON.parse(localStorage.getItem('username'));
 
-            if (userFromLS && userFromLS === username) {
-                setUserSuccess(true);
-            } else {
-                setUserErrors(true);
-                setHelperTextUser(
-                    'The e-mail address does not exist, please try again'
-                );
-            }
+        if (userFromLS && userFromLS === username) {
+            setUserSuccess(true);
+        } else {
+            setUserSuccess(false);
+            setHelperTextUser(
+                'The e-mail address does not exist, please try again'
+            );
         }
     };
 
@@ -50,14 +47,14 @@ export default function UserInput({ onGetUserSuccess, isSignIn }) {
             fullWidth
             size="small"
             required
-            error={userErrors ? true : false}
-            helperText={userErrors ? helperTextUser : ''}
+            error={userSuccess === false ? true : false}
+            helperText={userSuccess === false ? helperTextUser : ''}
             onBlur={
                 isSignIn
                     ? () => validateUserSignIn()
                     : () => validateUserSignUp()
             }
-            onFocus={() => setUserErrors(false)}
+            onFocus={() => setUserSuccess(null)}
             value={username}
             onChange={onChange}
             sx={{ mb: 3 }}
